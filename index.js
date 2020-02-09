67fc16441a4fb3bad349a52e8d26f063854ade71
Vamsi Aila <vamsi.aila@rapidops.com>
2020-02-09 22:50:33 +0530

    asdfghjkl

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-// mongoose.connect('mongodb+srv://grapher:rapidops123456@grapher-pwytq.mongodb.net/graph_blog?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb://localhost:27017/graph-blog', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://grapher:rapidops123456@grapher-pwytq.mongodb.net/graph_blog?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/graph-blog', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
     console.log('MongoDB Connected');
     app.listen(8080, () => console.log('graphing on port 8080'));
