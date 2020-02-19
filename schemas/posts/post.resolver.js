c1e8ce963f7acf2df53fc70728bba713c752aca2
Vamsi Aila <vamsi.aila@rapidops.com>
2020-02-19 16:41:29 +0530

    bug fixings

--- schemas/posts/post.resolver.js
+++ schemas/posts/post.resolver.js
@@ -51,7 +51,7 @@ class PostResolver {
             throw new Error('Authorization Error');
         }
         const exist = await new PostsModel().posts.findOne({ Title: Post.Title }).lean().exec();
-        if(exist._id !== post._id) {
+        if(exist && (exist._id !== post._id)) {
             throw new Error('Title already exist');
         }
         post.Title = Post.Title;
